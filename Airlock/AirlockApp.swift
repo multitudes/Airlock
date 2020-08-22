13bbb50ddc05358048f43aa39188762f08600796
laurent <wrmultitudes@gmail.com>
2020-08-22 09:47:31 +0200

    Initial Commit

---
+++ Airlock/AirlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirlockApp.swift
+//  Airlock
+//
+//  Created by Laurent B on 22/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct AirlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
