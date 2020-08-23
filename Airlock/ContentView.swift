eb69e3b4b1561dc4893fa9fc40ebaebe77284fbe
laurent <wrmultitudes@gmail.com>
2020-08-23 16:13:22 +0200

    landing screen done with accessible colors!

--- Airlock/ContentView.swift
+++ Airlock/ContentView.swift
@@ -10,43 +10,76 @@ import SwiftUI
 struct ActivityRingView: View {
     @Binding var progress: CGFloat
     
-    var colors: [Color] = [Color.darkRed, Color.lightRed]
-    
     var body: some View {
-        ZStack {
-            Circle()
-                .stroke(Color.outlineRed, lineWidth: 40)
-            Circle()
-                .trim(from: 0.0, to: progress)
-                .stroke(
-                    AngularGradient(
-                        gradient: Gradient(colors: colors), center: .center,
-                        startAngle: .degrees(0), endAngle: .degrees(360)
-                    ),
-                    style: StrokeStyle(lineWidth: 40, lineCap: .round)
-                ).rotationEffect(.degrees(-90))
-            Circle()
-                .frame(width: 40, height: 40)
-                .foregroundColor(.darkRed)
-                .offset(y: -150)
-            Circle()
-                .border(Color.clear, width: 0)
-                .frame(width: 40, height: 40)
-                .offset(y: -150)
-                .foregroundColor(progress > 0.95 ? Color.lightRed: Color.lightRed.opacity(0))
-                .rotationEffect(Angle.degrees(360 * Double(progress)))
-                .shadow(color: progress > 0.95 ? Color.black.opacity(0.1): Color.clear, radius: 8, x: 20, y: 0)
-        }.frame(idealWidth:300, idealHeight: 300, alignment: .center)
+        VStack(alignment: .center, spacing: 40) {
+        
+                Text("Two Minutes Meditation")
+                    .font(Font.system(size: 60))
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                    .bold()
+                    .layoutPriority(1)
+                    .lineLimit(2)
+                    .frame(maxWidth: 350, alignment: .center)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+
+                Text("Observe your breath")
+                    .frame(minWidth: 300, alignment: .center)
+                    .font(.title)
+                    .foregroundColor(Color.lightTitle)
+                    .layoutPriority(1)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.5)
+            ZStack {
+                Circle()
+                    .stroke(Color.outlineLightRed, lineWidth: 30)
+                Circle()
+                    .trim(from: 0.0, to: progress)
+                    .stroke(
+                        Color.white,
+                        style: StrokeStyle(lineWidth: 30, lineCap: .round)
+                    ).rotationEffect(.degrees(-90))
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(progress != 0.0 ? .white : Color.clear  )
+                    .offset(y: -150)
+                Circle()
+                    .border(Color.clear, width: 0)
+                    .frame(width: 30, height: 30)
+                    .offset(y: -150)
+                    .foregroundColor(progress >= 0.95 ? Color.white : Color.clear)
+                    .rotationEffect(Angle.degrees(360 * Double(progress)))
+                    .shadow(color: progress > 0.95 ? Color.black.opacity(0.3): Color.clear, radius: 1, x: 1, y: 0)
+                
+                Button(action: {
+                    printHello()
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: 250, height: 250)
+                            .foregroundColor(progress == 0.0 ? .buttonLightRed : Color.clear)
+                        Text("Start")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    
+                })
+            }.frame(idealWidth:300, idealHeight: 300, alignment: .center)
+            }
+        }
+        
+    func printHello() {
+        print("hello")
     }
-    
 }
 
 struct ContentView: View {
-    @State private var progress: CGFloat = 1
+    @State private var progress: CGFloat = 0.00
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color(hex: "F83480"), Color(hex: "FA1859")]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [Color.gradientStartRed, Color.gradientEndRed]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             ActivityRingView(progress: $progress)
                 .fixedSize()
