a0c3d82443193f4515973202d6e899b201a45157
laurent <wrmultitudes@gmail.com>
2020-08-27 16:01:08 +0200

    got some progress turning the circle

--- Airlock/ContentView.swift
+++ Airlock/ContentView.swift
@@ -8,70 +8,45 @@
 import SwiftUI
 
 struct ActivityRingView: View {
-    @State private var timerIsOn: Bool = false
-    @Binding var progress: CGFloat
+    @Binding var timerIsOn: Bool
+    @Binding var progress: Int
     var frameSize: CGFloat?
     
     var body: some View {
         
         ZStack {
             ZStack {
-                
+                Text("on! \(Double(progress)/120.0)")
                 Circle()
                     .stroke(Color.outlineLightRed, lineWidth: frameSize! / 5)
                 Circle()
-                    .trim(from: 0.0, to: progress)
+                    .trim(from: 0.0, to: CGFloat(Double(progress) / 120.0))
                     .stroke(
                         Color.white,
                         style: StrokeStyle(lineWidth: frameSize! / 5, lineCap: .round)
                     )
                     .border(Color.clear, width: 0)
                     .rotationEffect(.degrees(-90))
+                    //.animation(Animation.linear(duration: 10))
                 Circle()
                     .border(Color.clear, width: 0)
                     .frame(width: frameSize! / 5  , height: frameSize! / 5)
-                    .foregroundColor(progress != 0.0 ? .white : Color.clear  )
+                    .foregroundColor(progress != 0 ? .white : Color.clear  )
                     .offset(y: -(frameSize! / 2 )   )
                 Circle()
                     .border(Color.clear, width: 0)
                     .frame(width: frameSize! / 5, height: frameSize! / 5)
                     .offset(y: -frameSize! / 2 )
-                    .foregroundColor(progress >= 0.95 ? Color.white : Color.clear)
-                    .rotationEffect(Angle.degrees(360 * Double(progress)))
-                    .shadow(color: progress >= 0.94 ? Color.black.opacity(0.3): Color.clear, radius: frameSize! / 250 , x: frameSize! / 150, y: 0)
+                    .foregroundColor(Double(progress / 120) >= 0.95 ? Color.white : Color.clear)
+                    .rotationEffect(Angle.degrees(360 * Double(progress / 120)))
+                    .shadow(color: Double(progress / 120) >= 0.94 ? Color.black.opacity(0.3): Color.clear, radius: frameSize! / 250 , x: frameSize! / 150, y: 0)
                 
                 
             }.frame(idealWidth: frameSize!, idealHeight: frameSize!, alignment: .center)
             
-            Button(action: {
-                printHello()
-                timerIsOn.toggle()
-            }, label: {
-                if timerIsOn {
-                    ZStack {
-                        Circle()
-                            .frame(width: 250, height: 250)
-                            .foregroundColor(progress == 0.0 ? .buttonLightRed : Color.clear)
-                        Text("Cancel")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                    }
-                    .accessibility(label: Text("Start two minutes"))
-                } else {
-                    ZStack {
-                        Circle()
-                            .frame(width: 250, height: 250)
-                            .foregroundColor(progress == 0.0 ? .buttonLightRed : Color.clear)
-                        Text("Start")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                    }
-                    .accessibility(label: Text("Start two minutes"))
-                }
-            })
+            
         }
     }
-    
     
     func printHello() {
         print("hello")
@@ -79,7 +54,10 @@ struct ActivityRingView: View {
 }
 
 struct ContentView: View {
-    @State private var progress: CGFloat = 0.95
+    
+
+    @State var progress: Int = 0
+    @State var isOn: Bool = false
     
     var body: some View {
         ZStack {
@@ -87,6 +65,19 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             GeometryReader { geometry in
+               
+                if isOn {
+                    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+                    Text("on! \(progress)")
+                        .onReceive(timer) { _ in
+                            if progress < 120 {
+                                progress += 1
+                            } else {
+                                progress = 0
+                                isOn = false
+                            }
+                        }.frame(minWidth: geometry.size.width, alignment: .center)
+                }
                 
                 Text("Two Minutes Meditation")
                     .font(.largeTitle)
@@ -99,14 +90,43 @@ struct ContentView: View {
                     .frame(minWidth: geometry.size.width, alignment: .center)
                     .minimumScaleFactor(0.5)
                     .multilineTextAlignment(.center)
-                ActivityRingView(progress: $progress, frameSize: geometry.size.width / 1.5 )
+                ActivityRingView(timerIsOn: $isOn, progress: $progress, frameSize: geometry.size.width / 1.5 )
                     .fixedSize()
                     .position(x: geometry.size.width / 2 , y: geometry.size.height / 2)
+                PushButton(isOn: $isOn, size: geometry.size.width)
+                    .position(x: geometry.size.width / 2 , y: isOn ? geometry.size.height / 2 : geometry.size.height / 2 + geometry.size.height / 2.6)
+                    .animation(.easeInOut(duration: 0.5))
+                
             }
             
+ 
             
         }
         
+    }
+}
+
+struct PushButton: View {
+    @Binding var isOn: Bool
+    let title: String = "Start"
+    var size: CGFloat
+    var onColors = [Color.gradientStartRed, Color.buttonLightRed]
+    
+    var body: some View {
+        Button(action: {
+            self.isOn.toggle()
+
+        }, label: {
+            Text(isOn ? "Cancel" : "Start")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .frame(width: isOn ? size / 2 : size / 2 , height: isOn ? size / 2 : size / 5 )
+                .background(LinearGradient(gradient: Gradient(colors: onColors ), startPoint: .top, endPoint: .bottom))
+                .clipShape(Capsule())
+                .foregroundColor(.white)
+                .shadow(color: Color.red ,radius: 5)
+                .animation(.easeInOut(duration: 0.7))
+        }).accessibility(label: isOn ? Text("Cancel") : Text("Start 2 minutes meditation"))
     }
 }
 
