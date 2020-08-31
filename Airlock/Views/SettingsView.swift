fa0ab0c8f889d3e19c7c6c0df2bbb51ddf24be83
laurent <wrmultitudes@gmail.com>
2020-08-31 13:41:32 +0200

    phwew fixed almost a bug
    with the feedback compose email. It was crashing at runtime because of a missing parenthese.. but runtime only! This would have been tough to fix but done!

--- Airlock/Views/SettingsView.swift
+++ Airlock/Views/SettingsView.swift
@@ -116,8 +116,9 @@ struct SettingsView: View {
                             .alert(isPresented: self.$alertNoMail) {
                                 Alert(title: Text("Please set up your email account on your Apple device to send a feedback"))
                             }
-                            MailView(result: self.$result)
-                            }
+                            
+                        }
+                            
                             
                             NavigationLink(destination: AboutThisApp()) {
                                 HStack {
