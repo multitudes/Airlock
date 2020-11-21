6bd55c72cb1940bc480dc23a2d33a4ccd6ac48de
laurent <wrmultitudes@gmail.com>
2020-11-21 18:21:17 +0100

    added context menu

--- Airlock/Views/Buttons/PushButton.swift
+++ Airlock/Views/Buttons/PushButton.swift
@@ -15,30 +15,37 @@ func log(_ message: String, _ file: String = #file, _ line:Int = #line, _ functi
 }
 
 struct PushButton: View {
-    @Binding var isOn: Bool
-    @Binding var progress: CGFloat
-    
-    let title: String = "Start"
-    var size: CGFloat
-    var onColors = [Color.gradientStartRed, Color.buttonLightRed]
-    
-    var body: some View {
-        Button(action: {
-            isOn.toggle()
+	@Binding var isOn: Bool
+	@Binding var progress: CGFloat
+
+	let title: String = "Start"
+	var size: CGFloat
+	var onColors = [Color.gradientStartRed, Color.buttonLightRed]
+
+	var body: some View {
+		Button(action: {
+			isOn.toggle()
 			//log("Button pressed")
-            if !isOn {
-                progress = 0.0
-            }
-        }, label: {
-            Text(isOn ? "Cancel" : "  Start  ") // the whitespaces in "Start" are there to avoid animation irregularities
+			if !isOn {
+				progress = 0.0
+			}
+		}, label: {
+			Text(isOn ? "Cancel" : "  Start  ") // the whitespaces in "Start" are there to avoid animation irregularities
 		})
+		.padding()
 		.font(.system(size: 44, weight: .bold, design: .rounded))
 		.foregroundColor(.white)
+		.contentShape(RoundedRectangle(cornerRadius: 20))
 		.animation(.easeOut(duration: 0.8))
 		.keyboardShortcut(.space, modifiers: [])
 		.accessibility(label: isOn ? Text("Cancel") : Text("Start 2 minutes meditation"))
 		.contextMenu {
-			Button(action: {}, label: { Text("Easter Egg coming to you")})
+			if !isOn {
+				Text("You can choose between:")
+				Button("Standard: 2 minutes"){}
+				Button("Medium: 20 minutes"){}
+				Button("Longer: 60 minutes!"){}
+			}
 		}
-    }
+	}
 }
